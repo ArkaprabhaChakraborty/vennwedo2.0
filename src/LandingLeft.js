2a86985230a98874b7cc6c4fc4f06dbe49a4271a
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-02-17 00:04:36 +0530

    left pane changes

--- src/LandingLeft.js
+++ src/LandingLeft.js
@@ -5,8 +5,18 @@ import './Landing.css'
 function LandingLeft() {
     return (
             <div className="landing_left">
-                <div className="description card">
-                    <img className="vennwedo_logo" src=""alt=""/>
+                <div className="description_card">
+                    <img className="vennwedo_logo" src="https://raw.githubusercontent.com/ArkaprabhaChakraborty/vennwedo2.0/a7aae554d9d6b05cad6e5f9c78a8678defd18093/assets/vennwedo.svg"alt=""/>
+                    <div className="card_title">
+                        <p>Video Streamer</p>
+                    </div>
+                    <div className="card_text">
+                        <p> 
+                            Stream and Broadcast 
+                            High Quality Video 
+                            anywhere everywhere 
+                        </p>
+                    </div>
                 </div>
             </div>
     )
