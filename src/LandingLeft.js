193d8dc0abe75f7fc0fd4feece7852a60dff832a
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-02-16 11:27:39 +0530

    landing left pane added

---
+++ src/LandingLeft.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Landing.css'
+function LandingLeft() {
+    return (
+            <div className="landing_left">
+                description comes here
+            </div>
+    )
+}
+
+export default LandingLeft
