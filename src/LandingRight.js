3a61e852ce2a3dcd9e8c5078a1fe3aa38aa1196c
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-02-16 11:27:50 +0530

    landing right pane added

---
+++ src/LandingRight.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Landing.css'
+function LandingRight() {
+    return (
+        <div className="landing_right">
+            Svgs come here
+        </div>
+    )
+}
+
+export default LandingRight 
