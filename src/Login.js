b922462a1e490f4d0bb56c561240deece36c75e8
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-02-17 12:30:45 +0530

    login js created

---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Login.css";
+function Login() {
+    return (
+        <div className="loginpage">
+            <video className="background_webm" src="" loop autoPlay muted>
+                <source src="" type='webm'/>
+            </video>
+        </div>
+    )
+}
+
+export default Login
