50f6b50e567eef9389a0aab1c393d11bb29e7178
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-02-17 12:30:28 +0530

    login added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import LandingLeft from './LandingLeft';
 import LandingRight from './LandingRight';
+import Login from './Login';
 
 function App() {
   return (
     <div className="app">
-      <div className="app_page">      
-       <LandingLeft/>
-       <LandingRight/>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <div className="app_page">      
+              <LandingLeft/>
+              <LandingRight/>
+            </div>
+          </Route>
+          <Route path="/login">
+            <div className="app_page">
+              <Login/>
+            </div>
+          </Route>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
