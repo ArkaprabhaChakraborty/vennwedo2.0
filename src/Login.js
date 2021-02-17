4cc267c53eabb99c806185b023c96118d61fc76f
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-02-17 17:52:54 +0530

    login js final

--- src/Login.js
+++ src/Login.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import "./Login.css";
 function Login() {
     return (
         <div className="loginpage">
-            <video className="background_webm" src="" loop autoPlay muted>
-                <source src="" type='webm'/>
-            </video>
+            <Particles className="particles"
+                params={{
+	            "particles": {
+	            "number": {
+	                "value": 100
+	                },
+	            "size": {
+	                "value": 4
+	                },
+                "color":
+                    {
+                        "value":"#00FFFF"
+                    },
+                    "links":
+                    {
+                        "color":"#00FFFF"
+                    }
+	            },
+	            "interactivity": {
+	                "events": {
+	                    "onhover": {
+	                        "enable": true,
+	                    "mode": "repulse"
+	                    }
+	                }
+	            }
+	            }} />
+            <div className="logincard">
+                <img className="logo"src="https://raw.githubusercontent.com/ArkaprabhaChakraborty/vennwedo2.0/main/assets/vennwedo.svg" alt=""/>
+                <input className="email_input" placeholder="EMAIL"/>
+                <br/>
+                <input className="email_input" placeholder="PASSWORD"/>
+                <br/>
+                <button className="button">Login</button>
+            </div>
         </div>
     )
 }
