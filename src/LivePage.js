63080080fae8d14d347c3692d3b980a9fb930b44
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-02-19 20:58:15 +0530

    livepage modifs

--- src/LivePage.js
+++ src/LivePage.js
@@ -1,16 +1,28 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from './Header'
 import './LivePage.css'
 import RecommendedVideos from './RecommendedVideos'
+import SearchPage from './SearchPage'
 import Sidebar from './Sidebar'
 function LivePage() {
     return (
         <div>
+            <Router>
             <Header/>
             <div className="livepage">
                 <Sidebar/>
-                <RecommendedVideos/>
+            
+            <Switch>
+                <Route path="/live" exact>
+                    <RecommendedVideos/>
+                </Route>
+                <Route path="/live/search/:searchTerm">
+                    <SearchPage/>
+                </Route>
+            </Switch>
             </div>
+            </Router>
         </div>
     )
 }
